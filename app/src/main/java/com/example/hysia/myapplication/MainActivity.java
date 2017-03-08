1ab9c4a01b111b6b4abfd9aa0e8a441f9428226e
zum <hysia.assist@gmail.com>
2017-03-08 09:48:24 +0900

    [ hysia ] develop 코드 수정

--- app/src/main/java/com/example/hysia/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hysia/myapplication/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.hysia.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout mFlSubView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFlSubView = (FrameLayout) findViewById(R.id.main_ll_subview);
+
+        TestView testView = new TestView(this);
+        mFlSubView.addView(testView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+
     }
 }
---
+++ app/src/main/java/com/example/hysia/myapplication/TestView.java
@@ -0,0 +1,16 @@
+package com.example.hysia.myapplication;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by hysia on 2017. 3. 8..
+ */
+
+public class TestView extends View {
+
+    public TestView(Context context) {
+        super(context);
+    }
+
+}
