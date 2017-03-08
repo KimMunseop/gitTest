ca009b0416283236963fcf603075569f9e6b98d4
zum <hysia.assist@gmail.com>
2017-03-08 09:56:21 +0900

    [ hysia] 3way merge Test를 위한 commit

---
+++ app/src/main/java/com/example/hysia/myapplication/Test3wayMerge2.java
@@ -0,0 +1,11 @@
+package com.example.hysia.myapplication;
+
+/**
+ * Created by hysia on 2017. 3. 8..
+ */
+
+public class Test3wayMerge2 {
+    public Test3wayMerge2() {
+        // 테스트를 위한 클래스
+    }
+}
